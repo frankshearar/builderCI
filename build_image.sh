b92f5657586db96b5341e7dc7bb5d2063e065d20
Dale Henrichs <dhenrich@vmware.com>
2012-06-11 15:57:13 -0700

    use Squeak4.3 instead of SqueakCore4.3 (avoid Issue #7)

--- build_image.sh
+++ build_image.sh
@@ -17,12 +17,12 @@ case "$ST" in
     unzip PharoCore-1.3-13328.zip
     cd PharoCore-1.3
   ;;
-  # SqueakCore4.3
+  # Squeak4.3
   Squeak4.3)
     cd $IMAGES_PATH
-    wget http://ftp.squeak.org/4.3/SqueakCore4.3.zip
-    unzip SqueakCore4.3.zip
-    cd SqueakCore4.3
+    wget http://ftp.squeak.org/4.3/Squeak4.3.zip
+    unzip Squeak4.3.zip
+    cd Squeak4.3
     wget http://ftp.squeak.org/4.1/SqueakV41.sources.gz
     gunzip SqueakV41.sources.gz
   ;;
