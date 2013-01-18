077015ee6bd6bfd3531e495c0fb1364f94e83ee3
Dale Henrichs <dhenrich@vmware.com>
2013-01-18 14:21:26 -0800

    issue #27: fix testBuilderCI script and add one more test run

--- testBuilderCI.sh
+++ testBuilderCI.sh
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2012 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
 #
+
+#run tests
 ./build.sh -i $ST -m -f "$BUILDER_CI_HOME/tests/travisCI.st" -o travisCI
 if [[ $? != 0 ]] ; then exit 1; fi
 cd "${BUILD_PATH}/travisCI/"
@@ -13,6 +15,8 @@ $BUILDER_CI_HOME/buildTravisStatusCheck.sh # dump Transcript on failed tests and
 if [[ $? != 0 ]] ; then exit 1; fi
 cat TravisTranscript.txt
 
+# make sure that system runs okay when you skip the metacello bootstrap step
+cd $BUILDER_CI_HOME
 ./build.sh -i $ST -X -f "$BUILDER_CI_HOME/tests/skipMetacelloBootstrap.st" -o travisCI
 if [[ $? != 0 ]] ; then exit 1; fi
 cd "${BUILD_PATH}/travisCI/"
@@ -21,3 +25,8 @@ if [[ $? != 0 ]] ; then exit 1; fi
 $BUILDER_CI_HOME/buildTravisStatusCheck.sh 
 if [[ $? != 0 ]] ; then exit 1; fi
 cat TravisTranscript.txt
+
+# make sure that the testTravisCI.sh script runs without error ... no tests run
+cd $BUILDER_CI_HOME
+./testTravisCI.sh
+if [[ $? != 0 ]] ; then exit 1; fi
