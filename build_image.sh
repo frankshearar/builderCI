cb0b22da502eeb5f9324dff5bd53aad49e5ea3db
Dale Henrichs <dhenrich@vmware.com>
2013-01-18 11:55:28 -0800

    Issue #27: quiet options afor atp-get and wget

--- build_image.sh
+++ build_image.sh
@@ -15,7 +15,7 @@
 case "$(uname -m)" in
         "x86_64")
                 echo "64bit os"
-                sudo apt-get install libc6:i386
+                sudo apt-get -qq install libc6:i386
                 ;;
         *)
                 echo "32 vit os"
@@ -30,14 +30,14 @@ case "$ST" in
   # PharoCore-1.0
   PharoCore-1.0)
     cd $IMAGES_PATH
-    wget https://gforge.inria.fr/frs/download.php/26832/PharoCore-1.0.zip
+    wget -q https://gforge.inria.fr/frs/download.php/26832/PharoCore-1.0.zip
     unzip PharoCore-1.0.zip
     cd PharoCore-1.0
   ;;
   # PharoCore-1.1
   PharoCore-1.1)
     cd $IMAGES_PATH
-    wget https://gforge.inria.fr/frs/download.php/28341/PharoCore-1.1.2.zip
+    wget -q https://gforge.inria.fr/frs/download.php/28341/PharoCore-1.1.2.zip
     unzip PharoCore-1.1.2.zip
     cd PharoCore-1.1.2
     IMAGE_BASE_NAME=PharoCore-1.1.2-11422
@@ -45,7 +45,7 @@ case "$ST" in
   # PharoCore-1.2
   PharoCore-1.2)
     cd $IMAGES_PATH
-    wget https://gforge.inria.fr/frs/download.php/28553/PharoCore-1.2.2-12353.zip
+    wget -q https://gforge.inria.fr/frs/download.php/28553/PharoCore-1.2.2-12353.zip
     unzip PharoCore-1.2.2-12353
     cd PharoCore-1.2.2-12353
     IMAGE_BASE_NAME=PharoCore-1.2.2-12353
@@ -53,41 +53,41 @@ case "$ST" in
   # PharoCore-1.3
   PharoCore-1.3)
     cd $IMAGES_PATH
-    wget https://gforge.inria.fr/frs/download.php/30567/PharoCore-1.3-13328.zip
+    wget -q https://gforge.inria.fr/frs/download.php/30567/PharoCore-1.3-13328.zip
     unzip PharoCore-1.3-13328.zip
     cd PharoCore-1.3
   ;;
   # Pharo-1.4
   Pharo-1.4)
     cd $IMAGES_PATH
-    wget https://gforge.inria.fr/frs/download.php/31259/Pharo-1.4-14557.zip
+    wget -q https://gforge.inria.fr/frs/download.php/31259/Pharo-1.4-14557.zip
     unzip Pharo-1.4-14557
     cd Pharo-1.4
   ;;
   # Pharo-2.0
   Pharo-2.0)
     cd $IMAGES_PATH
-    wget https://ci.lille.inria.fr/pharo/view/Pharo%202.0/job/Pharo-2.0/lastSuccessfulBuild/artifact/Pharo-2.0.zip
+    wget -q https://ci.lille.inria.fr/pharo/view/Pharo%202.0/job/Pharo-2.0/lastSuccessfulBuild/artifact/Pharo-2.0.zip
     unzip Pharo-2.0.zip
     cd Pharo-2.0
   ;;
   # Squeak-4.3 ... allow Squeak4.3 for backwards compatibility
   Squeak-4.3|Squeak4.3)
     cd $IMAGES_PATH
-    wget http://ftp.squeak.org/4.3/Squeak4.3.zip
+    wget -q http://ftp.squeak.org/4.3/Squeak4.3.zip
     unzip Squeak4.3.zip
     cd Squeak4.3
-    wget http://ftp.squeak.org/4.1/SqueakV41.sources.gz
+    wget -q http://ftp.squeak.org/4.1/SqueakV41.sources.gz
     gunzip SqueakV41.sources.gz
     IMAGE_BASE_NAME=Squeak4.3
     ;;
   # Squeak-4.4
   Squeak-4.4)
     cd $IMAGES_PATH
-    wget http://www.squeakci.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/*zip*/target.zip
+    wget -q http://www.squeakci.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/*zip*/target.zip
     unzip target.zip
     cd target
-    wget http://ftp.squeak.org/4.1/SqueakV41.sources.gz
+    wget -q http://ftp.squeak.org/4.1/SqueakV41.sources.gz
     gunzip SqueakV41.sources.gz
     IMAGE_BASE_NAME=TrunkImage
     ;;
