f236de3d213ce0ca01379094b669a324a788ac65
Dale Henrichs <dhenrich@vmware.com>
2013-01-18 13:15:15 -0800

    Issue #27: better non-zero exit status checking?

--- testBuilderCI.sh
+++ testBuilderCI.sh
@@ -4,20 +4,14 @@
 #
 # Copyright (c) 2012 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
 #
-./build.sh -i $ST -m -f "$BUILDER_CI_HOME/tests/travisCI.st" -o travisCI
-[ $? -q 0 ] exit($?) 
+./build.sh -i $ST -m -f "$BUILDER_CI_HOME/tests/travisCI.st" -o travisCI || exit $?
 cd "${BUILD_PATH}/travisCI/"
-$BUILDER_CI_HOME/buildImageErrorCheck.sh
-[ $? -q 0 ] exit($?) 
-$BUILDER_CI_HOME/buildTravisStatusCheck.sh
-[ $? -q 0 ] exit($?) 
+$BUILDER_CI_HOME/buildImageErrorCheck.sh || exit $?
+$BUILDER_CI_HOME/buildTravisStatusCheck.sh || exit $?
 cat TravisTranscript.txt
 cd $BUILDER_CI_HOME
-./build.sh -i $ST -X -f "$BUILDER_CI_HOME/tests/skipMetacelloBootstrap.st" -o travisCI
-[ $? -q 0 ] exit($?) 
+./build.sh -i $ST -X -f "$BUILDER_CI_HOME/tests/skipMetacelloBootstrap.st" -o travisCI || exit $?
 cd "${BUILD_PATH}/travisCI/"
-$BUILDER_CI_HOME/buildImageErrorCheck.sh
-[ $? -q 0 ] exit($?) 
-$BUILDER_CI_HOME/buildTravisStatusCheck.sh
-[ $? -q 0 ] exit($?) 
+$BUILDER_CI_HOME/buildImageErrorCheck.sh || exit $?
+$BUILDER_CI_HOME/buildTravisStatusCheck.sh || exit $?
 cat TravisTranscript.txt
