ca0e434e7ed096f6dfd6859e05b08054b3e786c0
Dale Henrichs <dhenrich@vmware.com>
2013-01-18 13:05:39 -0800

    Issue #27: have to check exit status on each command ...

--- testBuilderCI.sh
+++ testBuilderCI.sh
@@ -5,13 +5,19 @@
 # Copyright (c) 2012 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
 #
 ./build.sh -i $ST -m -f "$BUILDER_CI_HOME/tests/travisCI.st" -o travisCI
+[ $? -q 0 ] exit($?) 
 cd "${BUILD_PATH}/travisCI/"
 $BUILDER_CI_HOME/buildImageErrorCheck.sh
+[ $? -q 0 ] exit($?) 
 $BUILDER_CI_HOME/buildTravisStatusCheck.sh
+[ $? -q 0 ] exit($?) 
 cat TravisTranscript.txt
 cd $BUILDER_CI_HOME
 ./build.sh -i $ST -X -f "$BUILDER_CI_HOME/tests/skipMetacelloBootstrap.st" -o travisCI
+[ $? -q 0 ] exit($?) 
 cd "${BUILD_PATH}/travisCI/"
 $BUILDER_CI_HOME/buildImageErrorCheck.sh
+[ $? -q 0 ] exit($?) 
 $BUILDER_CI_HOME/buildTravisStatusCheck.sh
+[ $? -q 0 ] exit($?) 
 cat TravisTranscript.txt
