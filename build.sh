f7c7c00a7a43096405f04e0018b473abb6decda7
Dale Henrichs <dhenrich@vmware.com>
2013-02-27 13:37:15 -0800

    Issue #38: fix syntax error in script

--- build.sh
+++ build.sh
@@ -216,7 +216,7 @@ if [ $pid ] ; then
 		let "tictoc = $COUNTER % 60"
 		if [ "$tictoc" -eq 0 ] ; then
                		echo "travis ... be patient PLEASE: https://github.com/dalehenrich/builderCI/issues/38"
-                if 
+                fi 
 	done
         wait $pid
         exitStatus=$?
