3ffc6c03a96f3ea871856358dac82a517f4512a6
Dale Henrichs <dhenrich@vmware.com>
2013-03-01 16:50:40 -0800

    taskset experiment continued

--- build_image.sh
+++ build_image.sh
@@ -52,6 +52,6 @@ esac
 # only GemStone gets here
 uname -a 
 source /opt/gemstone/product/seaside/defSeaside #set GemStone environment variables
-taskset -c 0,1 0x00000003 startGemstone
+taskset -c 0,1 startGemstone
 gslist -lc
 
