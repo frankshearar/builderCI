f3e7898ae7db47024a24133c28eb6918bd9e2083
Dale Henrichs <dhenrich@vmware.com>
2013-03-02 08:55:15 -0800

    borrow maglev license to see if I can get past cpu_affinity issues into more interesting territory ... if this works, then I will probably generate special purpose travis CCI licenses

--- build_image.sh
+++ build_image.sh
@@ -18,29 +18,24 @@ case "$ST" in
         exit 0
   ;;
   GemStone-2.4.4.1)
-        ./installGemstone.sh 2.4.4.1
-	if [[ $? != 0 ]] ; then exit 1; fi
-  ;;
+        GEMSTONE_VERSION_NAME="2.4.4.1"
+ ;;
   GemStone-2.4.4.7)
-        ./installGemstone.sh 2.4.4.7
-        if [[ $? != 0 ]] ; then exit 1; fi
-  ;;
+        GEMSTONE_VERSION_NAME="2.4.4.7"
+ ;;
   GemStone-2.4.5)
-        ./installGemstone.sh 2.4.5
-        if [[ $? != 0 ]] ; then exit 1; fi
-  ;;
+        GEMSTONE_VERSION_NAME="2.4.5"
+ ;;
   GemStone-2.4.5)
-        ./installGemstone.sh 2.4.5.1
-        if [[ $? != 0 ]] ; then exit 1; fi
-  ;;
+        GEMSTONE_VERSION_NAME="2.4.5.1"
+ ;;
   GemStone-3.0.1)
-        ./installGemstone.sh 3.0.1
-        if [[ $? != 0 ]] ; then exit 1; fi
-  ;;
+        GEMSTONE_VERSION_NAME="3.0.1"
+ ;;
   GemStone-3.1.0.2)
-        ./installGemstone.sh 3.1.0.2
-        if [[ $? != 0 ]] ; then exit 1; fi
-  ;;
+        GEMSTONE_VERSION_NAME="3.1.0.2"
+        KEY_PATH="./gemstone/etc/maglev.demo.key-Linux-x86_64"
+ ;;
 
 
   # unknown
@@ -51,8 +46,12 @@ esac
 
 # only GemStone gets here
 uname -a 
+./installGemstone.sh $GEMSTONE_VERSION_NAME
+if [[ $? != 0 ]] ; then exit 1; fi
 source /opt/gemstone/product/seaside/defSeaside #set GemStone environment variables
-taskset -c 0 startGemstone
+chmod +w  /opt/gemstone/product/seaside/etc/gemstone.key
+cp $KEY_PATH /opt/gemstone/product/seaside/etc/gemstone.key
+startGemstone
 gslist -lc
 cat /opt/gemstone/log/seaside.log
 
