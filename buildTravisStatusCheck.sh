bfb85e1241f43688e748bf8b85851cd12e4e5d5a
Dale Henrichs <dhenrich@vmware.com>
2013-03-06 12:05:06 -0800

    add -verbose arg to turn on/off verbose logging

--- buildTravisStatusCheck.sh
+++ buildTravisStatusCheck.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #
+# checks for build success/failure and dumps appropriate file to stdou
+#
+#      -verbose flag causes unconditional transcript display
+#
 # Copyright (c) 2012 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
 #
 if ( test -e TravisCISuccess.txt); then 
+  if [ "$1" = "-verbose" ] ; then
+    ls -altr TravisTranscript.txt
+    cat TravisTranscript.txt
+  fi
   cat TravisCISuccess.txt
 fi
 if ( test -e TravisCIFailure.txt ); then 
--- testTravisCI.sh
+++ testTravisCI.sh
@@ -2,6 +2,8 @@
 #
 # Test driver script for builderCI itself
 #
+#      -verbose flag causes unconditional transcript display
+#
 # Copyright (c) 2012 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
 #
 ./build.sh -i $ST -m -f "$PROJECT_HOME/tests/travisCI.st" -o travisCI
@@ -9,9 +11,5 @@ if [[ $? != 0 ]] ; then exit 1; fi
 cd "${BUILD_PATH}/travisCI/"
 $BUILDER_CI_HOME/buildImageErrorCheck.sh # dump Transcript on error and exit
 if [[ $? != 0 ]] ; then exit 1; fi
-$BUILDER_CI_HOME/buildTravisStatusCheck.sh # dump Transcript on failed tests and exit
+$BUILDER_CI_HOME/buildTravisStatusCheck.sh "$@" # dump Transcript on failed tests and exit
 if [[ $? != 0 ]] ; then exit 1; fi
-# reach here on successful build
-# cat TravisTranscript.txt
-ls -altr TravisTranscript.txt
-cat TravisTranscript.txt
