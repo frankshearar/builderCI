f0d4ee41f0e11db244de40906551a6559d4b12c8
Dale Henrichs <dhenrich@vmware.com>
2013-03-06 12:20:31 -0800

    label the rails

--- buildTravisStatusCheck.sh
+++ buildTravisStatusCheck.sh
@@ -9,26 +9,26 @@
 if ( test -e TravisCISuccess.txt); then 
   echo "SUCCESS: $(basename $0)"
   if [ "$1" = "-verbose" ] ; then
-    echo "--------------------------------------------------------------------"
+    echo "---TRANSCRIPT-----------------------------------------------------------------"
     ls -altr TravisTranscript.txt
     cat TravisTranscript.txt
-    echo "--------------------------------------------------------------------"
+    echo "---TRANSCRIPT-----------------------------------------------------------------"
   fi
   cat TravisCISuccess.txt
   exit 0
 fi
 if ( test -e TravisCIFailure.txt ); then 
   echo "FAILURE: $(basename $0)"
-  echo "--------------------------------------------------------------------"
+  echo "---TRANSCRIPT-----------------------------------------------------------------"
   cat TravisTranscript.txt
-  echo "--------------------------------------------------------------------"
+  echo "---TRANSCRIPT-----------------------------------------------------------------"
   cat TravisCIFailure.txt
   exit 1
 fi
 echo "neither SUCCESS nor FAILURE: $(basename $0)"
 if [ "$1" = "-verbose" ] ; then
-  echo "--------------------------------------------------------------------"
+  echo "---TRANSCRIPT-----------------------------------------------------------------"
   ls -altr TravisTranscript.txt
   cat TravisTranscript.txt
-  echo "--------------------------------------------------------------------"
+  echo "---TRANSCRIPT-----------------------------------------------------------------"
 fi
