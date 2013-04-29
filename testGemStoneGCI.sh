edfc7d2783e59318179a4a1ef5cd2dc817705aca
Dale Henrichs <dhenrich@vmware.com>
2013-04-29 14:57:56 -0700

    revert .travis.yml to non-gci version a little logging for testGemStoneGCI.sh

--- testGemStoneGCI.sh
+++ testGemStoneGCI.sh
@@ -18,6 +18,10 @@ if [[ $? != 0 ]] ; then
   exit 1
 fi
 echo "Starting Client: $CLIENT"
+pwd
+ls
+cd $BUILDER_CI_HOME
+ls
 ST="$CLIENT"
 ./build.sh -i $ST -m -f "$PROJECT_HOME/tests/clientGCI.st" -o travisCI
 if [[ $? != 0 ]] ; then 
