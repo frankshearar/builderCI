fdc18d5fc1533f2df154d2a09dff6b6cb567e012
Dale Henrichs <dhenrich@vmware.com>
2013-04-29 17:36:18 -0700

    [ci skip] need error checking after GemStone build ... test suite is run in both server and client

--- testGemStoneGCI.sh
+++ testGemStoneGCI.sh
@@ -17,6 +17,11 @@ if [[ $? != 0 ]] ; then
   $BUILDER_CI_HOME/dumpTranscript.sh
   exit 1
 fi
+cd "${BUILD_PATH}/travisCI/"
+$BUILDER_CI_HOME/buildImageErrorCheck.sh # dump Transcript on error and exit
+if [[ $? != 0 ]] ; then exit 1; fi
+$BUILDER_CI_HOME/buildTravisStatusCheck.sh "$@" # dump Transcript on failed tests and exit
+if [[ $? != 0 ]] ; then exit 1; fi
 echo "Starting Client: $CLIENT"
 cd $BUILDER_CI_HOME
 ST="$CLIENT"
