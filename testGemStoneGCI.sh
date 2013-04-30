18a5173ea05d7f7faa6b35ac229cf69966d5fb86
Dale Henrichs <dhenrich@vmware.com>
2013-04-29 17:20:02 -0700

    [ci skip] spelunking for testGemStoneGCI.sh

--- testGemStoneGCI.sh
+++ testGemStoneGCI.sh
@@ -23,6 +23,10 @@ ST="$CLIENT"
 ./build.sh -i $ST -m -f "$PROJECT_HOME/tests/clientGCI.st" -o travisCI
 if [[ $? != 0 ]] ; then 
   echo "ERROR: $(basename $0)"
+  ls -altr $BUILD_PATH
+  ls -altr $BUILD_PATH/travisCI
+  ls -altr $BUILD_PATH/builds/
+  ls -altr $BUILD_PATH/builds/travisCI
   cd "${BUILD_PATH}/travisCI/"
   $BUILDER_CI_HOME/buildImageErrorCheck.sh # dump Transcript on error and exit
   if [[ $? != 0 ]] ; then exit 1; fi
